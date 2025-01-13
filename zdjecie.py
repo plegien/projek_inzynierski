e8a7481ab7a731cad0ba363e5335380b61cc3699
plegien <legienpiotr@gmail.com>
2025-01-13 23:49:15 +0100

    Delete zdjecie.py

--- zdjecie.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img = cv2.imread("test.jpg")
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv,(10, 100, 20), (25, 255, 255) )
-cv2.imshow("orange", mask);cv2.waitKey();cv2.destroyAllWindows()
